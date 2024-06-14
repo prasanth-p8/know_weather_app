2073aa6be10f245b136458b7d0264a863b206947
prasanthberlin <prasanthberlin@gmail.com>
2024-06-14 23:26:01 +0530

    app started

--- src/App.js
+++ src/App.js
@@ -1,22 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import { CiLight } from "react-icons/ci";
+import { MdOutlineDarkMode } from "react-icons/md";
+import "./App.css";
 
 function App() {
+  const [darkTheme, setDarkTheme] = useState(false);
+
+  const changeTheme = () => {
+    setDarkTheme(!darkTheme);
+  };
+
+  const bgColor = darkTheme ? "dark-bg" : "light-bg";
+  const websiteImage = darkTheme
+    ? "https://res.cloudinary.com/dlefoxknm/image/upload/v1718387557/dark-website-logo_ez47g9.png"
+    : "https://res.cloudinary.com/dlefoxknm/image/upload/v1718382826/know-weather-website-logo.png";
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className={`app-main-container ${bgColor}`}>
+      <header className="header-container">
+        <div>
+          <img src={websiteImage} alt="website logo" className="website-logo" />
+          <h1 className="website-name">Know Weather</h1>
+        </div>
+        <div>
+          <button
+            onClick={changeTheme}
+            className={`theme-button ${darkTheme ? "dark" : "light"}`}
+          >
+            {darkTheme ? (
+              <MdOutlineDarkMode size={35} color="#00abee" />
+            ) : (
+              <CiLight size={35} color="#fffb1c" />
+            )}
+          </button>
+        </div>
       </header>
     </div>
   );
